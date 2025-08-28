7b46ee30169e3d2de2b210662fe9c7e7e59e4c82
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-08-28 13:04:56 +0200

    social media card component

--- src/app/components/layout/Footer.js
+++ src/app/components/layout/Footer.js
@@ -1,3 +1,27 @@
+import SocialMediaCard from "../common/SocialMediaCard";
+import styles from "./Footer.module.css";
+
 export default function Footer() {
-  return <footer>things!</footer>;
+  return (
+    <footer className={styles.footerContainer}>
+      <p>Reach us!</p>
+
+      <div className={styles.socialMedia}>
+        <SocialMediaCard
+          staticIcon="/social-media/whatsapp.png"
+          animatedIcon="/social-media/whatsapp.gif"
+          link="https://wa.me/+27737953918?text=Hi%20there,%20I'm%20interested%20in%20your%20services."
+        ></SocialMediaCard>
+
+        <SocialMediaCard
+          staticIcon="/social-media/whatsapp.png"
+          animatedIcon="/social-media/whatsapp.gif"
+          link="https://wa.me/+27737953918?text=Hi%20there,%20I'm%20interested%20in%20your%20services."
+        ></SocialMediaCard>
+        <p>insta</p>
+        <p>whatsapp</p>
+        <p>email</p>
+      </div>
+    </footer>
+  );
 }
