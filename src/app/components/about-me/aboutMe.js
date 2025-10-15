1474fd188ab1ddb7545245f0ebc748ad25827b73
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-10-15 15:45:50 +0200

    product main setup

--- src/app/components/about-me/aboutMe.js
+++ src/app/components/about-me/aboutMe.js
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function AboutMe() {
   return (
     <div className={styles.aboutMeContainer}>
-      <div className={styles.information}>
+      <div className={styles.context}>
         <div className={styles.title}>
           <SectionCard title={"About Me"} />
           <div>
--- src/app/components/products/products.js
+++ src/app/components/products/products.js
@@ -1,6 +1,8 @@
 import PriceList from "../common/PriceList";
 import SectionCard from "../common/SectionCard";
 import ThreeCakePops from "../common/ThreeCakePops";
+import styles from "./Products.module.css";
+import Image from "next/image";
 
 const standardPrice = [
   { name: "Small", qt: 15, standard: 225, deluxe: 300 },
@@ -16,34 +18,52 @@ const fancyPrice = [
 
 export default function Products() {
   return (
-    <div>
-      <SectionCard title={"Products"}></SectionCard>
-      <ThreeCakePops></ThreeCakePops>
+    <div className={styles.container}>
+      <div>
+        <Image
+          src="/./cake-pops/flower.jpeg"
+          alt="cake pops"
+          width={300}
+          height={300}
+        ></Image>
+      </div>
 
-      <p>
-        All Vanilla Cake Pops have a chocolate coating, which you can fully
-        customize! Choose your coating color, add shimmer and sprinkles in the
-        colors you want. With the Fancy option you can choose a decoravtive
-        desing, based on your event&apos;s theme.
-      </p>
+      <div className={styles.context}>
+        <div className={styles.title}>
+          <SectionCard title={"Products"}></SectionCard>
+          <ThreeCakePops></ThreeCakePops>
+        </div>
 
-      <PriceList title={"Standard"} data={standardPrice}></PriceList>
-      <PriceList title={"Fancy"} data={standardPrice}></PriceList>
+        <div className={styles.text}>
+          <p>
+            All Vanilla Cake Pops have a chocolate coating, which you can fully
+            customize! Choose your coating color, add shimmer and sprinkles in
+            the colors you want. With the Fancy option you can choose a
+            decoravtive desing, based on your event&apos;s theme.
+          </p>
 
-      <p>
-        <span>*</span> The Deluxe option includes packaging, featuring
-        individual cake pops that are wrapped and have a sticker
-      </p>
-      <p>
-        If your order exceeds 50 cake pops, I&apos;d be happy to create a custom
-        quote for you. Please contact me to get started!
-      </p>
-      <p>
-        I love custom projects! If you have a specific idea or a design in mind,
-        reach out and let&apos;s collaborate to create something unique
-      </p>
+          <div className={styles.tables}>
+            <PriceList title={"Standard"} data={standardPrice}></PriceList>
+            <PriceList title={"Fancy"} data={fancyPrice}></PriceList>
+          </div>
 
-      <button>Dowload price list</button>
+          <p>
+            <span>*</span> The Deluxe option includes packaging, featuring
+            individual cake pops that are wrapped and have a sticker
+          </p>
+          <p>
+            If your order exceeds 50 cake pops, I&apos;d be happy to create a
+            custom quote for you. Please contact me to get started!
+          </p>
+          <p>
+            I love custom projects! If you have a specific idea or a design in
+            mind, reach out and let&apos;s collaborate to create something
+            unique
+          </p>
+        </div>
+
+        <button>Dowload price list</button>
+      </div>
     </div>
   );
 }
