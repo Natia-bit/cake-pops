18bc44d2d7658454d273905a97e234f00075763c
Nadia N <82657219+Natia-bit@users.noreply.github.com>
2025-11-11 16:11:00 +0200

    download button and minor fixes (#7)
    Download button and minor fixes and added pricelist

---
+++ src/app/components/common/Button.js
@@ -0,0 +1,9 @@
+import styles from "./Button.module.css";
+
+export default function Button({ title }) {
+  return (
+    <button className={styles.btn}>
+      <span>{title}</span>
+    </button>
+  );
+}
--- src/app/components/products/products.js
+++ src/app/components/products/products.js
@@ -7,6 +7,7 @@ import ImageGallery from "../ImageGallery";
 import Image from "next/image";
 import TwoColumnSection from "../common/TwoColumnSection";
 import MainTitle from "../common/MainTitle";
+import Button from "../common/Button";
 
 const standardPrice = [
   { name: "Small", qt: 15, standard: 225, deluxe: 300 },
@@ -22,13 +23,13 @@ const fancyPrice = [
 
 export default function Products() {
   return (
-    <div className={styles.container}>
+    <div>
       <div className={styles.mobileTitle}>
         <MainTitle title={"Products"} />
       </div>
       <TwoColumnSection
         sectionLeft={
-          <div className={styles.priceWapper}>
+          <div className={styles.priceWrapper}>
             <PriceCard
               title={"Standard"}
               data={standardPrice}
@@ -51,16 +52,19 @@ export default function Products() {
 
             <div className={styles.text}>
               <p>
-                <span>*</span> The Deluxe option includes packaging, featuring
-                individual cake pops that are wrapped and have a sticker
-              </p>
-              <p>
                 All Vanilla Cake Pops have a chocolate coating, which you can
-                fully customize! Choose your coating color, add shimmer and
-                sprinkles in the colors you want. With the Fancy option you can
-                choose a decoravtive desing, based on your event&apos;s theme.
+                fully customize!
               </p>
 
+              <p>
+                Choose your coating color, add shimmer and sprinkles in the
+                colors you want.
+              </p>
+
+              <p>
+                With the Fancy option you can choose a decoravtive desing, based
+                on your event&apos;s theme.
+              </p>
               <p>
                 If your order exceeds 50 cake pops, I&apos;d be happy to create
                 a custom quote for you. Please contact me to get started!
@@ -68,11 +72,20 @@ export default function Products() {
               <p>
                 I love custom projects! If you have a specific idea or a design
                 in mind, reach out and let&apos;s collaborate to create
-                something unique
+                something unique.
+              </p>
+
+              <p className={styles.note}>
+                <span>*</span> The Deluxe option includes packaging, featuring
+                individual cake pops that are wrapped and have a sticker.
               </p>
             </div>
 
-            <button>Dowload price list</button>
+            <div className={styles.btnWrapper}>
+              <a href="/files/Cakepops-by-Leanne.jpeg" download="Cake Pops">
+                <Button title={"Dowload Price List"} />
+              </a>
+            </div>
           </div>
         }
       ></TwoColumnSection>
