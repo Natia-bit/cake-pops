817c63e08d1aa92780bd95412c3ba61a2cd77cc1
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-11-24 15:56:24 +0200

    added new image in the gallery

--- src/app/components/gallery/Gallery.js
+++ src/app/components/gallery/Gallery.js
@@ -7,15 +7,20 @@ export default function Gallery({ id }) {
   const data = [
     "/cake-pops/flowers.jpg",
     "/cake-pops/springtime-one.png",
+    "/cake-pops/baby-feet-2.jpeg",
     "/cake-pops/green-dino.jpg",
     "/cake-pops/blue-and-orange-flowers.png",
     "/cake-pops/unicorn.jpg",
     "/cake-pops/unicorns-packed.jpg",
     "/cake-pops/unicorn3.jpg",
     "/cake-pops/spiderman-pops.jpg",
+    "/cake-pops/spider-man.jpeg",
     "/cake-pops/flower.jpeg",
+    "/cake-pops/hello kittie.jpeg",
+    "/cake-pops/school.jpeg",
     "/cake-pops/baby-gender.jpg",
     "/cake-pops/polka dot.png",
+    "/cake-pops/teeth.jpeg",
   ];
 
   return (
