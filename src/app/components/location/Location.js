121bb077ce77d14bf4168299081047dc89a2a8bf
Natia Natisvili <natianatisvili@GLaDOS.local>
2025-11-24 15:47:02 +0200

    add image to location

--- src/app/components/location/Location.js
+++ src/app/components/location/Location.js
@@ -1,15 +1,34 @@
 import styles from "./Location.module.css";
 import MainTitle from "../common/MainTitle";
+import TwoColumnSection from "../common/TwoColumnSection";
+import Image from "next/image";
 
 export default function Location({ id }) {
   return (
     <section id={id} className={styles.container}>
-      <MainTitle title={"Location"} />
+      <TwoColumnSection
+        sectionLeft={
+          <div>
+            <MainTitle title={"Location"} />
 
-      <div className={styles.text}>
-        <p>Collection from Muizenberg.</p>
-        <p>Delivery options available in Cape Town.</p>
-      </div>
+            <div className={styles.text}>
+              <p>Collection from Muizenberg.</p>
+              <p>Delivery options available in Cape Town.</p>
+            </div>
+          </div>
+        }
+        sectionRight={
+          <div className={styles.imageWrapper}>
+            <Image
+              src="/./cake-pops/bluey-and-bingo.jpeg"
+              alt="cake pops"
+              width={600}
+              height={400}
+              className={styles.image}
+            ></Image>
+          </div>
+        }
+      />
     </section>
   );
 }
